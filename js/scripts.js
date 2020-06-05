bd7e1277285239c13fe3e12d29701cb9e9572728
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-05 10:02:13 -0700

    create constants for user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  $("form#test").submit(function(event){
+    const name = $("#name").val();
+    const age = parseInt($("#age").val());
+    const interest = $("#interests").val();
+    const company = $("#company").val();
+    const radioAnswer = $("input:radio[name=robot-human]:checked").val();
+    const color = $("#color").val();
+    
+    event.preventDefault();
+  })
 
 });
