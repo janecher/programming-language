96cdbf8649b55a3ff4d2d9790f53c2f4973aa30c
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-05 10:57:19 -0700

    add branching to choose answer from user inputs

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,26 @@ $(document).ready(function(){
     const company = $("#company").val();
     const radioAnswer = $("input:radio[name=robot-human]:checked").val();
     const color = $("#color").val();
-    
+    let answer;
+
+    if(!age || age < 0 || age > 150){
+      alert("Incorrect age input");
+      return;
+    }
+    if(age < 20){
+      answer = "Python";
+    }
+    else if (company === "Microsoft" || interest === "2"){
+      answer = "C#";
+    }
+    else if(age >= 20 && age < 30){
+      if(interest === "1" && (company === "Google" || company === "Facebook")){
+        answer = "Python";
+      }
+      else if(interest === "3" || interest === "4"){
+        answer = "JavaScript"
+      }
+    }
     event.preventDefault();
   })
 
